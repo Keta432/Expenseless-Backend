3c4ea5097d9692a7aebad8c0c600070dd4733745
Keta432 <ketanmaini1@gmail.com>
2024-05-04 17:44:07 +0530

    changes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
-// const mongoURI="mongodb://localhost:27017/expenseless";
-const mongoURI="mongodb+srv://kmainibe22:JEtTNwnxghX8yLyp@cluster0.rzylnzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoURI="mongodb://localhost:27017/expenseless";
+// const mongoURI="mongodb+srv://kmainibe22:JEtTNwnxghX8yLyp@cluster0.rzylnzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
