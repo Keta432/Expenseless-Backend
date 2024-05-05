6a36d91faff0abbae1116f0386cdc55e95e2691e
Keta432 <ketanmaini1@gmail.com>
2024-05-05 23:08:09 +0530

    commit

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ connectToMongo();
 const app = express()
 const port = process.env.PORT|| 5000
 
-app.use(cors({
-  origin:["http://localhost:3000","https://expenseless-frontend.onrender.com/"]//https://expenseless-frontend.onrender.com
-}))
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/auth',require('./routes/auth'))
